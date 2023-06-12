c596b07369ccde5594c10d8b25d2f56e3f6b412f
hanistheman <96705412+hanistheman@users.noreply.github.com>
2023-06-12 13:02:01 -0500

    Add files via upload

---
+++ mySecondProgram.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+
+int main() {
+    /* Practicing with ints.This program I am taking two variables that have already been declared,
+    and swapping their values. I also tried out the \n command that is also in Python and other
+    coding languages, along with multiline comments, as well as combining both text and a variable. */
+    int a = 1;
+    int b = 2;
+    int swap_num;
+    swap_num = a;
+    // I am using swap num to hold the value of a before I overwrite it with the value of b.
+    a = b;
+    std::cout <<"The new value of a is " <<a << " and the new value of b is " << swap_num;
+    return 0;
+};
