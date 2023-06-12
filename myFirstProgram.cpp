a07ce9baabe010836ed722da8a49fb3e2f370484
hanistheman <96705412+hanistheman@users.noreply.github.com>
2023-06-12 12:46:39 -0500

    Add files via upload

---
+++ myFirstProgram.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+
+    // This program I created to help me do the simplest of tasks a programmer can do:
+    // Print "Hello World" to the screen.
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
