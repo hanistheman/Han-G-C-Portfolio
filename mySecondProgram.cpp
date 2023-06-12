c5141cc0c92c06cc034d0917a43901634d1c6046
hanistheman <96705412+hanistheman@users.noreply.github.com>
2023-06-12 13:03:11 -0500

    Update mySecondProgram.cpp

--- mySecondProgram.cpp
+++ mySecondProgram.cpp
@@ -3,7 +3,7 @@
 int main() {
     /* Practicing with ints.This program I am taking two variables that have already been declared,
     and swapping their values. I also tried out the \n command that is also in Python and other
-    coding languages, along with multiline comments, as well as combining both text and a variable. */
+    coding languages, along with multiline comments, as well as combining both text and a variable in this program. */
     int a = 1;
     int b = 2;
     int swap_num;
